5402901285c6e374b2132139ad83781324933fda
keenwon <semanwmj@yeah.net>
2015-08-13 11:14:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# generator-fede2  
+使用yo快速构建[fede2](https://github.com/keenwon/fede2)
+
+## Usage
+安装yo
+
+    npm install -g yo
+
+安装generator-fede2
+
+    npm install -g generator-fede2
+
+构建项目
+
+    yo fede2
---
+++ app/index.js
@@ -0,0 +1,85 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+
+    name: function () {
+        var done = this.async();
+
+        this.prompt({
+            type: 'input',
+            name: 'projectName',
+            message: 'name: ',
+            default: this.appname
+        }, function (answers) {
+            this.projectName = answers.projectName;
+            done();
+        }.bind(this));
+    },
+
+    version: function () {
+        var done = this.async();
+
+        this.prompt({
+            type: 'input',
+            name: 'projectVersion',
+            message: 'version: ',
+            default: '1.0.0'
+        }, function (answers) {
+            this.projectVersion = answers.projectVersion;
+            done();
+        }.bind(this));
+    },
+
+    description: function () {
+        var done = this.async();
+
+        this.prompt({
+            type: 'input',
+            name: 'projectDescription',
+            message: 'description: '
+        }, function (answers) {
+            this.projectDescription = answers.projectDescription;
+            done();
+        }.bind(this));
+    },
+
+    buildPackageFile: function () {
+        this.fs.copyTpl(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json'),
+            {
+                name: this.projectName,
+                version: this.projectVersion,
+                description: this.projectDescription
+            }
+        );
+    },
+
+    copyFiles: function () {
+        this.directory('files', './');
+    },
+
+    install: function () {
+        this.npmInstall([
+            'body-parser',
+            'del',
+            'express',
+            'gulp',
+            'gulp-compile-handlebars',
+            'gulp-copy',
+            'gulp-jshint',
+            'gulp-minify-css',
+            'gulp-rename',
+            'gulp-replace',
+            'gulp-shell',
+            'gulp-util',
+            'hbs',
+            'jshint-stylish',
+            'morgan',
+            'webpack',
+            'webpack-core',
+            'webpack-dev-middleware'
+        ], {'save': true});
+    }
+
+});
